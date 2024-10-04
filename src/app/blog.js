54370341af82ae3a4e562655390822ab817fe965
Christian Saravia <christiansaravia@Christians-MacBook-Air.local>
2024-10-04 11:52:08 +0200

    first commit

---
+++ src/app/blog.js
@@ -0,0 +1,10 @@
+export default function Blog() {
+  return (
+    <div>
+      <h1>Blog</h1>
+      <p>Welcome to my AI blog posts.</p>
+      {/* Blog posts will go here */}
+    </div>
+  );
+}
+
---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Developer Portfolio</h1>
+      <p>Here are some of my projects:</p>
+      {/* Project listing will go here */}
+    </div>
+  );
+}
+
